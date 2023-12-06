fd2c718090c975f94c5b125058fcff5e3f431e7e
Famouus Bernard <famousbernard@gmail.com>
2023-12-06 04:46:19 +0000

    typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,19 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw Error('Position outside range');
-  }
+  // Create a new ArrayBuffer with the specified length
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer, 0, length);
-  view.set([value], position);
+
+  // Create an Int8Array view of the buffer
+  const int8Array = new Int8Array(buffer);
+
+  // Check if the specified position is within range
+  if (position >= 0 && position < length) {
+    // Set the value at the specified position
+    int8Array[position] = value;
+  } else {
+    // Throw an error if the position is outside the range
+    throw new Error("Position outside range");
+  }
+
+  // Return the ArrayBuffer
   return buffer;
 }
