480ef08145709813597096de5925e064f5efb16a
Famouus Bernard <famousbernard@gmail.com>
2023-12-06 03:32:50 +0000

    updated

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
-function getStudentsByLocation(students, city) {
-  return students.filter(student => student.location === city);
-}
+const getStudentsByLocation = (students, city) => {
+  const arrObj = students.filter((funtion) => funtion.location === city);
+  return arrObj;
+};
 
 export default getStudentsByLocation;
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer, 0, length);
   view.set([value], position);
-  return buffer
+  return buffer;
 }
